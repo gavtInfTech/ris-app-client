1e9a380610ebb376e35b7ca2b444ea8a2fbbe40a
inovvan <novvan2002@gmail.com>
2023-07-18 09:21:49 +0300

    levels menu

--- src/components/waterLevels/LevelsMenu.jsx
+++ src/components/waterLevels/LevelsMenu.jsx
@@ -3,22 +3,23 @@ import { Link } from "react-router-dom";
 import './levelsMenu.css';
 
 export default function LevelMenu () {
-return ( <div className="content1234">
-<div className="item5">
-<button className="btn3"><a href="/levelsGp">
-    <img src="/images/levelGp.png" alt="meteo" ></img>
-    <br></br>Уровни воды на <br></br>гидропостах
-    </a></button>
-    </div>
-<div></div>
-<div></div>
-<div className="item6">
-<button className="btn3"><a href="/LevelsGu">
-    <img src="/images/levelGu.png" alt="level" ></img>
-    <br></br>Уровни воды на <br></br>гидроузлах
-    </a></button>
-    </div>
-<div></div>
+return ( <div className="container">
+
+<button className="btn">
+    <Link to={'/levelsGp'}>
+        <img src="/images/levelGp.png" alt="meteo" ></img>
+        <br></br>Уровни воды на <br></br>гидропостах
+    </Link>
+</button>
+
+
+<button className="btn">
+    <Link to={'/LevelsGu'}>
+        <img src="/images/levelGu.png" alt="level" ></img>
+        <br></br>Уровни воды на <br></br>гидроузлах
+    </Link>
+</button>
+
 </div>
 )
 }
