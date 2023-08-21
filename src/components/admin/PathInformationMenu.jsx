52bf2aef7152eb6fcbd1db55c06c3281946e6c75
inovvan <novvan2002@gmail.com>
2023-08-21 13:57:46 +0300

    map

--- src/components/admin/PathInformationMenu.jsx
+++ src/components/admin/PathInformationMenu.jsx
@@ -1,59 +1,126 @@
-import { React, useContext } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Box from '@mui/material/Box';
-import style from './style.module.css'
-import { Route, Routes, Link, useLocation} from 'react-router-dom';
-import PathInformationMap from './map/PathInformationMap';
-import Sib from '../sib/Sib';
+import { React, useContext, useEffect, useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+import style from "./style.module.css";
+import { Route, Routes, Link, useLocation } from "react-router-dom";
+import PathInformationMap from "./map/PathInformationMap";
+import Sib from "../sib/Sib";
+import { api } from "../../axiosConfig";
 
 export default function PathInformationMenu() {
-  const {pathname} = useLocation();
-  
+  const { pathname } = useLocation();
+  const [signs, setSigns] = useState([]);
+
+  useEffect(() => {
+    const getSigns = async () => {
+      try {
+        const res = await api.get("/signs/getAll");
+        setSigns(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getSigns();
+  }, []);
+
   return (
-    <Box sx={{ bgcolor: 'background.paper', position: 'absolute', height: '100%', width: '100%' }}>
-      <AppBar position="static" className={style.appBar} sx={{ bgcolor: 'SteelBlue', width: '100%' }}>
+    <Box
+      sx={{
+        bgcolor: "background.paper",
+        position: "absolute",
+        height: "100%",
+        width: "100%",
+      }}
+    >
+      <AppBar
+        position="static"
+        className={style.appBar}
+        sx={{ bgcolor: "SteelBlue", width: "100%" }}
+      >
         <Tabs
           value={pathname}
           indicatorColor="secondary"
           TabIndicatorProps={{
             style: {
-              height: 3
-            }
+              height: 3,
+            },
           }}
           textColor="inherit"
           variant="scrollable"
           allowScrollButtonsMobile
         >
-          <Tab sx={{ width: 300, height: 70, fontSize: 14 }} 
-            label="Днепр" 
-            value={`/path-information/dnepr`} 
-            to={`/path-information/dnepr`} 
-            component={Link} 
+          <Tab
+            sx={{ width: 200, height: 70, fontSize: 14 }}
+            label="Днепр"
+            value={`/path-information/dnepr`}
+            to={`/path-information/dnepr`}
+            component={Link}
           />
-          <Tab sx={{ width: 300, height: 70, fontSize: 14 }} 
-            label="Березина" 
-            value={`/path-information/berezina`} 
-            to={`/path-information/berezina`} 
-            component={Link} 
+          <Tab
+            sx={{ width: 200, height: 70, fontSize: 14 }}
+            label="Березина"
+            value={`/path-information/berezina`}
+            to={`/path-information/berezina`}
+            component={Link}
           />
-          <Tab sx={{ width: 300, height: 70, fontSize: 14 }} 
-            label="Припять" 
-            value={`/path-information/pripyat`} 
-            to={`/path-information/pripyat`} 
-            component={Link} 
+          <Tab
+            sx={{ width: 200, height: 70, fontSize: 14 }}
+            label="Западная Двина"
+            value={`/path-information/dvina`}
+            to={`/path-information/dvina`}
+            component={Link}
+          />
+          <Tab
+            sx={{ width: 200, height: 70, fontSize: 14 }}
+            label="Неман"
+            value={`/path-information/neman`}
+            to={`/path-information/neman`}
+            component={Link}
+          />
+            <Tab
+            sx={{ width: 200, height: 70, fontSize: 14 }}
+            label="Припять"
+            value={`/path-information/pripyat1`}
+            to={`/path-information/pripyat1`}
+            component={Link}
+          />
+               <Tab
+            sx={{ width: 200, height: 70, fontSize: 14 }}
+            label="Верхний участок реки Припять"
+            value={`/path-information/pripyat2`}
+            to={`/path-information/pripyat2`}
+            component={Link}
+          />
+             <Tab
+            sx={{ width: 200, height: 70, fontSize: 14 }}
+            label="Сож"
+            value={`/path-information/soj`}
+            to={`/path-information/soj`}
+            component={Link}
           />
         </Tabs>
       </AppBar>
 
-       
       <Routes>
-          <Route path="/dnepr" element={<PathInformationMap />} />
-          <Route path="/berezina" element={<PathInformationMap />} />
-          <Route path="/pripyat" element={<PathInformationMap />} />
+        <Route path="/dnepr" element={<PathInformationMap signs={signs} />} />
+        <Route
+          path="/berezina"
+          element={<PathInformationMap signs={signs} />}
+        />
+        <Route path="/dvina" element={<PathInformationMap signs={signs} />} />
+        <Route path="/neman" element={<PathInformationMap signs={signs} />} />
+        <Route
+          path="/pripyat1"
+          element={<PathInformationMap signs={signs} />}
+        />
+        <Route
+          path="/pripyat2"
+          element={<PathInformationMap signs={signs} />}
+        />
+        <Route path="/soj" element={<PathInformationMap signs={signs} />} />
       </Routes>
-        
     </Box>
   );
 }
--- src/components/admin/map/PathInformationMap.jsx
+++ src/components/admin/map/PathInformationMap.jsx
@@ -9,35 +9,97 @@ import {
   Polyline,
 } from "react-yandex-maps";
 import { Box } from "@mui/system";
+import { useLocation } from "react-router-dom";
 
 const mapState = { center: [54.133392, 27.577899], zoom: 7, controls: [] };
 
-export default function PathInformationMap() {
+export default function PathInformationMap(props) {
   const [map, setMap] = useState(mapState);
-  const [contentHeight, setContentHeight] = useState("auto");
+  const [contentHeight, setContentHeight] = useState();
+  const { pathname } = useLocation();
+  const [riverSigns, setRiverSigns] = useState([]);
+
+  useEffect(() => {
+    let signs = [];
+    // eslint-disable-next-line default-case
+    switch (pathname) {
+      case "/path-information/dnepr":
+        signs = props.signs.filter((sign) => sign.river === "Днепр");
+        break;
+      case "/path-information/berezina":
+        signs = props.signs.filter((sign) => sign.river === "Березина");
+        break;
+      case "/path-information/dvina":
+        signs = props.signs.filter((sign) => sign.river === "Западная Двина");
+        break;
+      case "/path-information/neman":
+        signs = props.signs.filter((sign) => sign.river === "Неман");
+        break;
+      case "/path-information/pripyat1":
+        signs = props.signs.filter((sign) => sign.river === "Припять");
+        break;
+      case "/path-information/pripyat2":
+        signs = props.signs.filter((sign) => sign.river === "Верхний участок реки Припять");
+        break;
+      case "/path-information/soj":
+        signs = props.signs.filter((sign) => sign.river === "Сож");
+        break;
+    }
+    setRiverSigns(signs);
+  }, [pathname, props.signs]);
+
+  const riverSignsMarks = riverSigns.map((item) => {
+    let contentBody =
+      "Описание: " +
+      item.description +
+      "<br>Соответсвие цвета: " +
+      (!item.place ? "Соответствует" : "Не соответствует") +
+      "<br>Соответсвие места: " +
+      (!item.size ? "Соответствует" : "Не соответствует") +
+      "<br>Видимость: " +
+      (!item.visibility ? "Соответствует" : "Не соответствует") +
+      "<br>Комментарий: " +
+      item.comment;
+    return (
+      <Placemark
+        geometry={[item.latitude, item.longitude]}
+        key={item.id}
+        properties={{ balloonContentBody: [contentBody] }}
+        modules={["geoObject.addon.balloon"]}
+        options={{
+          iconLayout: "default#image",
+          iconImageHref: `/images/pathInformationImages/${item.image}.png`,
+          iconImageSize: [30, 30],
+          iconImageOffset: [-15, -15],
+        }}
+      />
+    );
+  });
 
   const calculateMaxHeight = () => {
     const windowHeight = window.innerHeight;
-  
+
     if (window.innerWidth > 1200) {
       setContentHeight(windowHeight - 188);
     } else setContentHeight(null);
   };
-  
+
   useEffect(() => {
     calculateMaxHeight();
     window.addEventListener("resize", calculateMaxHeight);
-
   }, []);
 
   return (
     <Box className={styles.container}>
-      <Box sx={{ maxHeight: contentHeight + "px" }} className={styles.infoPart}>
-        
-      </Box>
+      <Box
+        sx={{ maxHeight: contentHeight + "px" }}
+        className={styles.infoPart}
+      ></Box>
       <Box className={styles.mapPart}>
         <YMaps>
-          <Map state={map} className={styles.map}></Map>
+          <Map state={map} className={styles.map}>
+            {riverSignsMarks}
+          </Map>
         </YMaps>
       </Box>
     </Box>
