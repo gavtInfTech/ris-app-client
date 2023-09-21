2b50f15cd1aa14382fe3cf64ef2c5e05c13d85b1
inovvan <novvan2002@gmail.com>
2023-09-21 12:38:08 +0300

    notices images

--- src/components/noticesUser/NoticesList.jsx
+++ src/components/noticesUser/NoticesList.jsx
@@ -7,6 +7,7 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { Box } from "@mui/system";
 import Style from "./style.module.css";
 import Pagination from "@mui/material/Pagination";
+import  Icon  from "@mui/material/Icon";
 
 export default function NoticesList(props) {
   const { data } = props;
@@ -24,24 +25,26 @@ export default function NoticesList(props) {
   return (
     <div>
       {visibleData.map((doc) => (
+        
         <Accordion sx={{ mt: 1 }} key={doc.id}>
-          <AccordionSummary expandIcon={<ExpandMoreIcon />} sx={{}}>
-            <Typography sx={{ ml: "20px", fontSize: "17px" }}>
+          <AccordionSummary>
+            { doc.cause.includes('Изменение СНО') &&  <Icon sx={{width: "30px", height: "40px"}}> <img src="/images/sno.png" /></Icon> }
+            { doc.cause.includes('Метеологические условия') && <Icon sx={{width: "40px", height: "40px"}}> <img src="/images/weatherNotices.png" /></Icon> }
+            { doc.cause.includes('Опасно для жизни') && <Icon sx={{width: "40px", height: "40px"}}> <img src="/images/dangerous.png" /></Icon> }
+            <Typography sx={{ ml: "20px", fontSize: "17px", display: "flex", alignItems: "center" }}>
               ({doc.date.toLocaleString().slice(0, 17)}) – {doc.site}
             </Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <Box sx={{}}>
-              <Typography className={Style.typography}>
-                <p className={Style.p}>Река: {doc.river}</p>
-              </Typography>
-              <Typography className={Style.typography}>
-                <p className={Style.p}>Причины:  {doc.cause}</p>
-              </Typography>
-              <Typography className={Style.typography}>
-                <p className={Style.p}>Содержание:  {doc.content}</p>
-              </Typography>
-            </Box>
+            <Typography className={Style.typography}>
+              <p className={Style.p}>Река: {doc.river}</p>
+            </Typography>
+            <Typography className={Style.typography}>
+              <p className={Style.p}>Причины: {doc.cause}</p>
+            </Typography>
+            <Typography className={Style.typography}>
+              <p className={Style.p}>Содержание: {doc.content}</p>
+            </Typography>
           </AccordionDetails>
         </Accordion>
       ))}
